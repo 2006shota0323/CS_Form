babeb2dbd36517a5bd71a1c4696f0805e0262cb8
Shota0323 <shotawatanabe03230@gamil.com>
2024-11-06 11:21:46 +0900

    testlabelに文字表示

--- CS_Form/CS_Form/Form1.cs
+++ CS_Form/CS_Form/Form1.cs
@@ -12,6 +12,9 @@ namespace CS_Form
 {
     public partial class Form1 : Form
     {
+        TestLabel _testLabel;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +22,12 @@ namespace CS_Form
 
             for (int i = 0; i < 10; i++)
             {
-                TestButton TestButton = new TestButton(i,(i%3)*100, (i/3)*100,100,100);
+                TestButton TestButton = new TestButton(this,i,(i%3)*100, (i/3)*100,100,100);
                 Controls.Add(TestButton);
             }
 
-            TestLabel testLabel = new TestLabel("ラベル",10, 300, 100, 500);
-            Controls.Add(testLabel);
+            _testLabel = new TestLabel("ラベル",100, 300, 100, 500);
+            Controls.Add(_testLabel);
 
 
 
@@ -33,16 +36,14 @@ namespace CS_Form
             // label.Text = "ラベル";
             //Controls.Add(label);
 
-
-
-
-
-
-
-
-
-
-
+        }
+        /// <summary>
+        /// ラベルの文字更新
+        /// </summary>
+        /// <param name="str"></param>
+        public void LabelTextUpdate(string str)
+        {
+            _testLabel.TextUpdate(str);
         }
     }
     
--- CS_Form/CS_Form/testbutton.cs
+++ CS_Form/CS_Form/testbutton.cs
@@ -10,11 +10,14 @@ namespace CS_Form
 {
      class TestButton:Button
     {
+        Form1 _form1;
 
 
-
-        public TestButton(int id,int x, int y, int width, int height)
+        public TestButton(Form1 form1,int id,int x, int y, int width, int height)
         {
+            _form1 = form1;
+
+
             //ClickイベントにOnClick関数を登録
             //ボタンをクリックした時に登録した関数を実行します
             Click += OnClick;
@@ -31,7 +34,7 @@ namespace CS_Form
 
         public void OnClick(object sender,EventArgs s)
         {
-            MessageBox.Show (Text);
+            _form1.LabelTextUpdate(Text);
         }
 
     }
