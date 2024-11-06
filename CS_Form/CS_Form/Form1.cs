846f2c12c1a0e5a19bf3b933afe5447c224c6f15
Shota0323 <shotawatanabe03230@gamil.com>
2024-11-06 10:37:44 +0900

    testlabel

--- CS_Form/CS_Form/Form1.cs
+++ CS_Form/CS_Form/Form1.cs
@@ -21,10 +21,28 @@ namespace CS_Form
             {
                 TestButton TestButton = new TestButton(i,(i%3)*100, (i/3)*100,100,100);
                 Controls.Add(TestButton);
-
-                
-
             }
+
+            TestLabel testLabel = new TestLabel("ラベル",10, 300, 100, 500);
+            Controls.Add(testLabel);
+
+
+
+            // Label label = new Label();
+            //label.Location = new Point(30, 400);
+            // label.Text = "ラベル";
+            //Controls.Add(label);
+
+
+
+
+
+
+
+
+
+
+
         }
     }
     
---
+++ CS_Form/CS_Form/TestLabel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.LinkLabel;
+using System.Windows.Forms;
+
+namespace CS_Form
+{
+     class TestLabel:Label
+    {
+        public TestLabel(string str, int x, int y, int width, int height)
+        {
+
+
+            //ボタン内に文字を表示させる
+            Text = str;
+
+            //ボタンの生成場所の指定
+            Location = new Point(x, y);
+
+            //ボタンの大きさを指定
+            Size = new Size(width, height);
+        }
+
+        
+
+
+
+
+
+
+    }
+}
