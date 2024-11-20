00fe0fd6daf0ba7db32dbcf36e786540f584677f
Shota0323 <shotawatanabe03230@gamil.com>
2024-11-20 10:20:52 +0900

    ボタン

--- CS_Form/CS_Form/Form1.cs
+++ CS_Form/CS_Form/Form1.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using CS_Form.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CS_Form
 {
@@ -14,32 +17,45 @@ namespace CS_Form
     {
         TestLabel _testLabel;
 
+        TestTextBox textBox;
+
+
 
         public Form1()
         {
             InitializeComponent();
 
             string[] q = new string[10] {"a","b","c","d","e","f","g","h","i","j"};
-                        
+             
+            
 
             for (int i = 0; i < 10; i++)
             {
                 TestButton TestButton = new TestButton(this, q[i],(i%3)*100, (i/3)*100,100,100);
                 Controls.Add(TestButton);
-                
             }
             
-            _testLabel = new TestLabel("ラベル表示",100, 300, 100, 500);
+            _testLabel = new TestLabel("ラベル表示",100, 300, 500, 100);
             Controls.Add(_testLabel);
 
+            textBox = new TestTextBox("テキストボックス",10,400,500,100);
 
+            Controls.Add(textBox);
 
             // Label label = new Label();
             //label.Location = new Point(30, 400);
             // label.Text = "ラベル";
             //Controls.Add(label);
 
+            
+
+
+
+
         }
+
+        
+
         /// <summary>
         /// ラベルの文字更新
         /// </summary>
@@ -48,6 +64,7 @@ namespace CS_Form
         {
             _testLabel.TextUpdate(str);
         }
+        
     }
     
 }
